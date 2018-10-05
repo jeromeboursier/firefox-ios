d28a56a7e8fe4ab07d2a96582057e389426d3a07
p.bertran <p.bertran@qwant.com>
2018-10-05 16:29:41 +0200

    "v2.1.0 - app_settings_options.2"

--- Client/Frontend/Settings/AppSettingsOptions.swift
+++ Client/Frontend/Settings/AppSettingsOptions.swift
@@ -750,7 +750,7 @@ class OpenSupportPageSetting: Setting {
 
     override func onClick(_ navigationController: UINavigationController?) {
         navigationController?.dismiss(animated: true) {
-            if let url = URL(string: "https://support.mozilla.org/products/ios") {
+            if let url = URL(string: "https://help.qwant.com/help/qwant-mobile/?client=qwantbrowser") {
                 self.delegate?.settingsOpenURLInNewTab(url)
             }
         }
@@ -909,7 +909,7 @@ class PrivacyPolicySetting: Setting {
     }
 
     override var url: URL? {
-        return URL(string: "https://www.mozilla.org/privacy/firefox/")
+        return URL(string: "https://about.qwant.com/legal/privacy/?client=qwantbrowser")
     }
 
     override func onClick(_ navigationController: UINavigationController?) {
